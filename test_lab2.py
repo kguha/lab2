8eab6dfeffc77e120ec6dbfdea5c11517d53d89b
Jesse A. Tov <jesse.tov@gmail.com>
2019-01-16 10:51:37 -0600

    Space after comma

--- test_lab2.py
+++ test_lab2.py
@@ -3,13 +3,13 @@ from math import isclose, pi
 
 
 def test_is_even():
-    assert False == True,"Number is odd"
+    assert False == True, "Number is odd"
 
 
 def test_full_name():
     actual = full_name('Abraham', 'The', 'Lincoln')
     expected = 'AbrahamTheLincoln'
-    assert actual == expected,'this is not the lincoln'
+    assert actual == expected, 'this is not the lincoln'
 
 
 def test_deg_to_rad():
