82d72899ee9bc827f89808ef940b4f0eca6e6d06
John Nguyen <johntnguyen9@gmail.com>
2019-01-15 18:30:48 -0600

    Add basic functions to lab2

--- lab2.py
+++ lab2.py
@@ -1,35 +1,43 @@
-#!/usr/bin/env python3
-from math import isclose, pi
-
-from geometry import *
+from math import pi
 
 from typing import NamedTuple
 
 
-# Write a Python program to calculate surface volume and area of a sphere.
-def calc_sphere():
-    radian = float(input('Radius of sphere: '))
-    sur_area = 4 * pi * radian ** 2
-    volume = (4 / 3) * (pi * radian ** 3)
-    print("Surface Area is: ", sur_area)
-    print("Volume is: ", volume)
+# Write a Python program to calculate surface volume and surface area of a sphere.
+def calc_sur_area(radius :float):
+    sur_area = 4 * pi * radius ** 2
+    return sur_area
 
 # Define a function that calculates the length of a string
-def str_len():
-    str = input("Please enter a string")
-    length = len(str)
-    return length
+def str_len(string: str):
+    print("Type your code here")
+    return string
 
 
 # Define a function that is your full name including first, middle
 # and last name
-def full_name():
-    first = input("Enter your first name")
-    mid = input("Enter your middle name")
-    last = input("Enter your last name")
+def full_name(first: str, mid: str, last: str):
     full = first + mid + last
     return full
 
 # Define a function that successfully tells you if your number is odd or even or divisible by your age
+def odd_or_even(num: int):
+    return
 
+# Define a function to convert degree to radian.
+def deg_to_rad(deg: float):
+    return
 
+def calc_vol(radius: float):
+    return
+
+class Sphere(NamedTuple):
+    """Represents a sphere positioned on a 3-D plane."""
+    radius: float = 0
+
+    # please define your volume function
+    def volume(self) -> float:
+        return calc_vol(self.radius)
+
+    def surface_area(self) -> float:
+        return calc_sur_area(self.radius)
